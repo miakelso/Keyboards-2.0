b93624a9ba3a389f8a49ff3df24c6f1e0d1485b4
miakelso <mia.kelso@tcu.edu>
2026-03-11 11:48:48 -0500

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -1,5 +1,5 @@
 // Set your deployed backend URL here once live.
 // Example: https://forge-server.onrender.com
 window.FORGE_API_URL = window.location.hostname === 'miakelso.github.io'
-  ? 'https://YOUR-BACKEND-URL.onrender.com'
+  ? 'https://keyboards-2-0.onrender.com'
   : 'http://localhost:8080';
