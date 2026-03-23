46e9b28733ff8ecfce8f3e71f584e305a7a1028a
miakelso <mia.kelso@tcu.edu>
2026-03-23 13:22:58 -0500

    3/23.1 (formspree)

--- api-config.js
+++ api-config.js
@@ -3,3 +3,7 @@
 window.FORGE_API_URL = window.location.hostname === 'miakelso.github.io'
   ? 'https://keyboards-2-0.onrender.com'
   : 'http://localhost:8080';
+
+// Optional Formspree endpoint (set this after creating a Formspree form).
+// Example: https://formspree.io/f/xxxxxxxx
+window.FORGE_FORMSPREE_ENDPOINT = 'https://formspree.io/f/mojkgqbj';
